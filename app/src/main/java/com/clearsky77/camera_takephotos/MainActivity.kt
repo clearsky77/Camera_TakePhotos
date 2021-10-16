fa9fa181ed47fff909cd75c94b5cc6db3656f56d
clearsky77 <clearsky3264@gmail.com>
2021-10-16 18:27:14 +0900

    카메라 권한 확인

--- app/src/main/java/com/clearsky77/camera_takephotos/MainActivity.kt
+++ app/src/main/java/com/clearsky77/camera_takephotos/MainActivity.kt
@@ -1,11 +1,39 @@
 package com.clearsky77.camera_takephotos
 
+import android.Manifest
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Toast
+import com.gun0912.tedpermission.PermissionListener
+import com.gun0912.tedpermission.normal.TedPermission
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        OpenCameraBtn.setOnClickListener {
+           val pl = object : PermissionListener {
+                // 권한이 허용 되었을 때. 실행한다.
+                override fun onPermissionGranted() {
+                    Toast.makeText(this@MainActivity, "권한 승인됨.", Toast.LENGTH_SHORT).show()
+                }
+                // 권한 거절 되었을 때.
+                override fun onPermissionDenied(deniedPermissions: MutableList<String>?) {
+                    Toast.makeText(this@MainActivity, "권한 거절됨.", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+            TedPermission.create()
+                .setPermissionListener(pl)
+                .setPermissions(Manifest.permission.CAMERA) // 카메라 권한
+                .check()
+        }
+
     }
+
 }
